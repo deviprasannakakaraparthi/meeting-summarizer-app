dd2592ce37f2ce49e9eed38ce9a80ad14215d899
prasanna <prasanna@DEVI>
2025-02-12 19:28:07 +0530

    Handles text summarization logic

---
+++ utils/summarizer.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+
+# Load pre-trained summarization model
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+def summarize_text(text):
+    summary = summarizer(text, max_length=150, min_length=50, do_sample=False)
+    return summary[0]['summary_text']
+
